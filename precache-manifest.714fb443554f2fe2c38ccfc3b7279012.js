f623a271002c74646df5ecaec97a00e170a2a1da
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-12 21:39:51 +0500

    Updates

---
+++ precache-manifest.714fb443554f2fe2c38ccfc3b7279012.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d743125ab84ab4d9647a5d54064204fd",
+    "url": "/habit-builder/index.html"
+  },
+  {
+    "revision": "295329306ac940b91ebd",
+    "url": "/habit-builder/static/css/main.40233ef7.chunk.css"
+  },
+  {
+    "revision": "dd2ac7945289153f9921",
+    "url": "/habit-builder/static/js/2.4a0f1b91.chunk.js"
+  },
+  {
+    "revision": "295329306ac940b91ebd",
+    "url": "/habit-builder/static/js/main.43ea56c4.chunk.js"
+  },
+  {
+    "revision": "c9f6e5f97b1d9497a1ef",
+    "url": "/habit-builder/static/js/runtime-main.72ddcfca.js"
+  }
+]);
